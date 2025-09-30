2282bc6e7f0588d49b7a037736b73695daf128e9
Van Dixon <vandixon@hotmail.com>
2025-09-29 21:19:10 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Van Dixon on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
